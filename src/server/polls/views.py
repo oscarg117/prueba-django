80f9c3f1715ef9e4c2bca801cf5376e29aac45b8
Oscar González <oscarg117@gmail.com>
2017-01-24 10:14:11 -0600

    Se agrega generación de PDF con pequeños errores.

--- src/server/polls/views.py
+++ src/server/polls/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import get_object_or_404, render
 
 from django.http import HttpResponseRedirect
+#***#
+from django.http import HttpResponse    #***#
+from django.template import Context     #***#
+from django.template.loader import get_template     #***#
+from subprocess import Popen, PIPE
+import tempfile
+import os
+#***#
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
@@ -50,4 +58,28 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after succesfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results_pdf', args=(question.id,)))
+
+
+def results_pdf(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question,
+    }
+    template = get_template('my_latex_template.tex')
+    #template = get_template('polls/my_latex_template.tex')
+    rendered_tpl = template.render(context).encode('utf-8')
+    with tempfile.TemporaryDirectory() as tempdir:
+        for i in range(2):
+            process = Popen(
+                ['pdflatex', '-output-directory', tempdir],
+                stdin=PIPE,
+                stdout=PIPE,
+            )
+            process.communicate(rendered_tpl)
+        with open(os.path.join(tempdir, 'texput.pdf'), 'rb') as f:
+            pdf = f.read()
+    r = HttpResponse(content_type='application/pdf')
+    #r['Content-Disposition'] = 'attachment; filename=texput.pdf'
+    r.write(pdf)
+    return r
